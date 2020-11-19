2a603cca931905300ac2171e08d7264403eefc1a
EricaReis <ericareis67@outlook.com>
2020-11-19 11:23:19 -0300

    repos

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function App() {
-  const [] = useState([]);
-  
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(async () => {
+    const response = await fetch('http://api.github.com/users/EricaReis/repos');
+    const data = await response.json();
+
+    setRepositories(data);
+  }, [])
+
+  useEffect(() => {
+    const filtered = repositories.filter(repo => repo.favorite);
+
+    document.title = `Você tem ${filtered.length} favoritos`
+  }, [repositories])
+
+  function handleAddRepository(){
+    setRepositories([...repositories, {id: Math.random(), name: 'Novo repo'}])
+  }
+
+  function handleFavorite (id){
+    const newRepositories = repositories.map(repo => {
+      return repo.id === id ? {...repo, favorite: !repo.favorite} : repo
+    });
+    setRepositories(newRepositories);
+  }
+
   return (
-    <h1> Hello World! </h1>
+    <>
+      <ul>
+        { repositories.map(repo => (
+        <li 
+        key={repo.id}>
+          {repo.name}
+          {repo.favorite && <span>(Favorito)</span>}
+          <button onClick={() => handleFavorite(repo.id)}>Favoritar</button>
+        </li>))}
+      </ul>
+      <button onClick={handleAddRepository}>
+        Adicionar repositório
+      </button>
+    </>
   );
 }
-
-
